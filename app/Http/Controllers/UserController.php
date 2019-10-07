066bf620f0fd6f6b29f71a819e4163cffe8f574c
khanhney <khanhney.dev@gmail.com>
2019-10-07 23:58:26 +0700

    inital project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function create(Request $req){
+        $user = new User();
+        $user->email    = $req->email;
+        $user->username = $req->username;
+        $user->password = $req->password;
+        
+        $user->save();
+        if($user){
+            $data = [
+                'error' => false,
+                'statusCode' => 200,
+                'data' => $user
+            ];
+        }else{
+            $data = [
+                'error' => true,
+                'statusCode' => 500,
+                'message' => "cannot_insert_data"
+            ];
+        }
+        return response()->json($data);
+    }
+
+    public function delete($id){
+        $user = User::find($id);
+        $user->delete();
+        if($user){
+            $data = [
+                'error' => false,
+                'statusCode' => 200,
+                'data' => $user
+            ];
+        }else{
+            $data = [
+                'error' => true,
+                'statusCode' => 500,
+                'message' => "cannot_delete_data"
+            ];
+        }
+
+        return response()->json($data);
+    }
+
+    public function update(Request $req, $id){
+        $email = $req->email;
+        $username = $req->username;
+        $password = $req->password;
+
+        $user = User::find($id);
+        $user->email =  $email;
+        $user->username = $username;
+        $user->password = $password;
+
+        $user->save();
+        if($user){
+            $data = [
+                'error' => false,
+                'statusCode' => 200,
+                'data' => $user
+            ];
+        }else{
+            $data = [
+                'error' => true,
+                'statusCode' => 500,
+                'message' => "cannot_update_data"
+            ];
+        }
+        return response()->json($data);
+    }
+
+    public function getList(){
+        $users = User::all();
+        if($users && count($users)){
+            $data = [
+                'error' => false,
+                'statusCode' => 200,
+                'data' => $users
+            ];
+        }else{
+            $data = [
+                'error' => true,
+                'statusCode' => 500,
+                'message' => "cannot_get_list_data"
+            ];
+        }
+        return response()->json($data);
+    }
+
+    public function getUserById($id){
+        $user = User::find($id);
+        if($user){
+            $data = [
+                'error' => false,
+                'statusCode' => 200,
+                'data' => $user
+            ];
+        }else{
+            $data = [
+                'error' => true,
+                'statusCode' => 500,
+                'message' => "cannot_get_info_data"
+            ];
+        }
+        return response()->json($data);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/users', 'UserController@getList');
+Route::get('/users/{id}', 'UserController@getUserById');
+Route::post('/users', 'UserController@create');
+Route::delete('/users/{id}', 'UserController@delete');
+Route::put('/users/{id}', 'UserController@update');
